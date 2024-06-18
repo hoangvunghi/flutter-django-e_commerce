e5d2f350de7c614aa01c70f7c6eda6dc54569d69
hoangvunghi <hoangvunghi2004@gmail.com>
2024-06-18 21:03:06 +0700

    u

--- backend/ecommerce/ecommerce/urls.py
+++ backend/ecommerce/ecommerce/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth.urls')),
+    path('api/', include('authen.urls')),
     path('api/', include('shopping.urls')),
 ]
